585c0aff71c52fcbc239c4d4d2f0cb8a1f1f5f77
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-01-12 11:28:30 +0400

    header onclick

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App({text,qrup}) {
+  const onclick = (e) =>{
+    const title = e.target.innerText;
+    alert(title)
+  }
   return (
     <div className="App">
-      <h1>{text}</h1>
+      <h1 onclick={onclick}>{text}</h1>
       <h3>{qrup}</h3>
     </div>
   );
