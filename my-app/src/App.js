3a10ad046f53a6784caf80ac1f719a34c439612d
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-01-13 10:11:33 +0400

    useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {useState} from "react";
+import {useEffect,useState} from "react";
 
 function App({text,age}) {
   const [student,setStudent] = useState({
@@ -15,13 +15,19 @@ function App({text,age}) {
       age:22
     })
   }
+  useEffect(()=>{
+    console.log("useEffecte daxil oldu ")
+    return ()=>{
+      console.log("return hisseye daxil oldu")
+    }
+  })
  
   return (
     <div className="App">
       <h1>{text}</h1>
-      <h3>{age}</h3>
+      <h3>{student.age}</h3>
       <h2>{student.name}</h2>
-      <button onclick={changeName}>Change Name</button>
+      <button onClick={changeName}>Change Name</button>
     </div>
   );
 }
