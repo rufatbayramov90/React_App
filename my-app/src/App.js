229b1b8c7055e54171605fe78afe91a697667d9f
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-01-13 09:42:46 +0400

    change name

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,29 @@
 
 import './App.css';
+import {useState} from "react";
 
-function App({text,qrup}) {
-  const [name,setName] = useState("Ilham");
+function App({text,age}) {
+  const [student,setStudent] = useState({
+    name:"Ilhan",
+    age:26,
+    qrup:"2.1"
+  });
   const changeName =()=>{
-    setName("Rufat");
+    setStudent({
+      ...student,
+      name:"Aysel",
+      age:22
+    })
   }
  
   return (
     <div className="App">
       <h1>{text}</h1>
-      <h3>{qrup}</h3>
+      <h3>{age}</h3>
+      <h2>{student.name}</h2>
       <button onclick={changeName}>Change Name</button>
     </div>
   );
 }
-
+ 
 export default App; 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App text="My first React proect" qrup="easy" />);
+root.render(<App text="My first React proect" age="32" />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
