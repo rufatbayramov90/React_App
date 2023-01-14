050a21213faf2a90a371ed4610aa70a4959d56a9
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-01-15 01:33:42 +0400

    Client routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,18 @@
 
 import './App.css';
 import {useEffect,useState} from "react";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
+import Contact from './components/Contact';
+import About from './components/About';
 
-function App( ) {
- 
- 
+function App( ) { 
   return (
     <div className="App">
+      <Link to="/elaqe">Kontakt sehifesine daxil ol</Link><br/>
+      <Link to="/haqqimizda">About sehifesine daxil ol</Link>
       <Routes>
-        <Route path='/elaqe' element={Contact} />
-        <Route path='/haqqimizda' element={About} />
+        <Route path="/elaqe" element={<Contact/>} />
+        <Route path="/haqqimizda" element={<About/>} />
       </Routes>      
     </div>
   );
