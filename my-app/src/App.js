7901af1d71ad8fe67b2db2f7853cd8eec40bca4d
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-01-15 01:08:19 +0400

    create compenents

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,46 +1,17 @@
 
 import './App.css';
 import {useEffect,useState} from "react";
+import { Route, Routes } from 'react-router-dom';
 
-function App({text,age}) {
-  const [student,setStudent] = useState({
-    name:"Ilhan",
-    age:26,
-    qrup:"2.1"
-  });
-  const[second,setSecond]=useState(0);
-  const[start,setStart]=useState(false);
-  const changeName =()=>{
-    setStudent({
-      ...student,
-      name:"Aysel",
-      age:22
-    })
-  }
-  const runTimer =()=>{
-   setStart(!start)
-  }
-  useEffect(()=>{
-    if(!start){
-      return;
-    }
-    const timer = setInterval(()=>{
-      setSecond(second + 1)
-    },1000);
-    return ()=>{
-      clearInterval(timer);
-    }
-  })
+function App( ) {
+ 
  
   return (
     <div className="App">
-      <h1>{text}</h1>
-      <h3>{student.age}</h3>
-      <h2>{student.name}</h2>
-      <button onClick={changeName}>Change Name</button>
-    
-      <p>{second}</p>
-      <button onClick={runTimer}>{start ? "Stop" : "Start"} Time</button>
+      <Routes>
+        <Route path='/elaqe' element={Contact} />
+        <Route path='/haqqimizda' element={About} />
+      </Routes>      
     </div>
   );
 }
